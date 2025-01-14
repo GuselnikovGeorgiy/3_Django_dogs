3fe7aa74d702998045a3e6fbf08dfd4f39ac2c20
Jorique <guselnikovgeorgiy@gmail.com>
2025-01-15 00:20:09 +0300

    added base dog serializer

--- Django_project/dogs/serializers.py
+++ Django_project/dogs/serializers.py
@@ -4,24 +4,23 @@ from rest_framework import serializers
 from dogs.models import Dog, Breed
 
 
-class DogListSerializer(ModelSerializer):
+class DogSerializer(ModelSerializer):
+    class Meta:
+        model = Dog
+        fields = "__all__"
+
+
+class DogListSerializer(DogSerializer):
     avg_breed_age = serializers.DecimalField(max_digits=3, decimal_places=2, read_only=True) # all dogs
 
-    class Meta:
-        model = Dog
-        fields = "__all__"
 
-
-class DogDetailSerializer(ModelSerializer):
+class DogDetailSerializer(DogSerializer):
     same_breed_count = serializers.IntegerField(read_only=True)  # one dog
-
-    class Meta:
-        model = Dog
-        fields = "__all__"
 
 
 class BreedSerializer(ModelSerializer):
     dogs_count = serializers.IntegerField(read_only=True)
+
     class Meta:
         model = Breed
         fields = "__all__"
